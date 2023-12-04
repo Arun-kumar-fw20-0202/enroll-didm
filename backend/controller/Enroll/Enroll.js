b8373819b10b42f2bb3a7f97653fdf9cbc7c7521
Arun Kumar ( Katill_Boyy ) <arun07744@gmail.com>
2023-12-04 15:21:15 +0530

    some upate

--- backend/controller/Enroll/Enroll.js
+++ backend/controller/Enroll/Enroll.js
@@ -2,21 +2,24 @@ const { enrollModal } = require("../../modal/Enroll/EnrollForm.modal");
 
 const enrollForm = async (req, res) => {
   const payload = req.body;
-  //   console.log(payload);
+  console.log(payload);
   try {
     const find = await enrollModal.findOne({ $or: [{ phone_number: payload.phone_number }, { email: payload.email }] });
-    console.log(find);
-    if (find) {
-      if (find.phone_number == payload.phone_number) {
-        return res.status(403).send({ msg: "Phone Number is alredy exist " });
-      } else if (find.email == payload.email) {
-        return res.status(403).send({ msg: "Email is alredy exist " });
-      }
-    } else {
-      const data = await enrollModal(payload);
-      await data.save();
-      return res.status(200).send({ msg: "data saved", data });
-    }
+    console.log(find, "find");
+    const data = await enrollModal(payload);
+    await data.save();
+    return res.status(200).send({ msg: "data saved", data });
+    // if (find) {
+    //   if (find.phone_number == payload.phone_number) {
+    //     return res.status(403).send({ msg: "Phone Number is alredy exist " });
+    //   } else if (find.email == payload.email) {
+    //     return res.status(403).send({ msg: "Email is alredy exist " });
+    //   }
+    // } else {
+    //   const data = await enrollModal(payload);
+    //   await data.save();
+    //   return res.status(200).send({ msg: "data saved", data });
+    // }
   } catch (err) {
     res.send(err.message).status(500);
   }
