0882caa1c4dcf91473b9d2d3f3ff53e608695739
Ghanishtkhurana <ghanishtkhurana9@gmail.com>
2023-12-06 11:39:54 +0530

    added the payment integration

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const Razorpay = require("razorpay");
 const shortid = require("shortid");
+const { enrollModal } = require("./modal/Enroll/EnrollForm.modal");
 // app.use(cors());
 
 const corsOptions = {
@@ -25,16 +26,36 @@ const razorpay = new Razorpay({
   key_secret: process.env.KeySecret,
 });
 
+app.post("/checkpayment", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const preObjFinder = await enrollModal.findOne({ _id: userId });
+    if (preObjFinder.paymentStatus) {
+      return res
+        .status(400)
+        .send({ message: "You already purchased the course", status: false });
+    }
+    return res.status(200).send({ message: "go ahead", status: true });
+  } catch (error) {
+    return res.status(400).send({ message: error.message, status: false });
+  }
+});
+
 app.post("/order", async (req, res) => {
-  const { amount, currency, receipt } = req.body;
+  const { amount, currency, userId } = req.body;
   const options = {
     amount: amount * 100, // Razorpay expects the amount in paise
     currency,
     receipt: shortid.generate(),
   };
   try {
+    // const preObjFinder = await enrollModal.findOne({ _id: userId });
+    // if (preObjFinder.paymentStatus) {
+    //   return res
+    //     .status(400)
+    //     .send({ message: "You already purchased the course" });
+    // }
     const order = await razorpay.orders.create(options);
-    console.log("response in backend=>", order);
     res.status(200).json({ message: "Payment done successfully", data: order });
   } catch (error) {
     return res.status(400).send({ message: error.message, status: false });
@@ -43,9 +64,29 @@ app.post("/order", async (req, res) => {
 
 app.post("/payment", async (req, res) => {
   try {
-    let { paymentId } = req.body;
-    console.log("paymentid=>", paymentId);
-    res.status(200).send({ message: "Payment done successfully" });
+    console.log("req.body=>", req.body);
+    const { userId, paid_amount, course_amount, paymentId } = req.body;
+    const preObjFinder = await enrollModal.findOne({ _id: userId });
+    if (preObjFinder.paymentStatus) {
+      return res
+        .status(400)
+        .send({ message: "You already purchased the course" });
+    }
+    const updateEnroll = await enrollModal.findByIdAndUpdate(
+      { _id: userId },
+      {
+        paid_amount: paid_amount / 100,
+        course_amount: course_amount,
+        paymentStatus: true,
+        paymentId: paymentId,
+      },
+      {
+        new: true,
+      }
+    );
+    res
+      .status(200)
+      .send({ message: "Payment done successfully", data: updateEnroll });
   } catch (error) {
     return res.status(400).send({ message: error.message, status: false });
   }
